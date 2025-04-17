9ddbb05fcae767d0af4bb05001437f7ab9be64d0
Maia Bard <Maiakbard@gmail.com>
2025-04-17 17:26:57 -0600

    Add code for index method to get all invoices of a specific merchant by the query parameter of a matching status

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -1,2 +1,9 @@
 class Api::V1::InvoicesController < ApplicationController
+  def index
+    status = params[:status] #grabs the value of the status query parameter - shipped, packaged, returned
+    merchant = Merchant.find(params[:merchant_id]) #looks up merchant with given merchant id frome the URL
+    invoices = merchant.invoices.where(status: status) #this will get all the merchants invoices that match the status being passed in
+
+    render json: InvoiceSerializer.new(invoices)
+  end
 end
