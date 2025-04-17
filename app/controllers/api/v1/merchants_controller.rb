954af1c31a2048ed9af748cb651686016bd63190
Maia Bard <Maiakbard@gmail.com>
2025-04-17 12:16:33 -0600

    Add tests for request response for deleting a merchant and test for cascading deletion of items associated with the merchant

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -6,16 +6,20 @@ class Api::V1::MerchantsController < ApplicationController
   end
 
   def destroy
-    merchant = Merhcant.find_by(id: params[:id])
-    
-    if merchant
+    # begin
+      merchant = Merchant.find(params[:id])
       merchant.destroy
       head :no_content
-    else 
-      render json: {
-        message: "Unable to delete merchant",
-        errors: ["Merchant ID #{params[:id]} is invalid"],
-      }, status: :not_found
+    # rescue ActiveRecord::RecordNotFound => error
+    #   render json: {
+    #     "errors": [
+    #       {
+    #       status: "404",
+    #       message: [error.message]
+    #       }
+    #     ]
+    #   }, status: :not_found
+    # end
   end 
 end
 
--- spec/requests/merchant_request_spec.rb
+++ spec/requests/merchant_request_spec.rb
@@ -1,3 +1,39 @@
 require "rails_helper"
 
 RSpec.describe "Merchant endpoints", type: :request do
+  describe 'destroy a merchant' do
+    it 'deletes a merchant and returns not content' do
+      merchant = Merchant.create!(name: "Test Merchant")
+
+      delete "/api/v1/merchants/#{merchant.id}"
+
+      expect(response).to have_http_status(:no_content)
+      expect(Merchant.find_by(id: merchant.id)).to be_nil
+    end
+
+    # it 'returns a not_found error' do
+    #   delete "/api/v1/merchants/99999"
+
+    #   expect(response).to have_http_status(:not_found)
+
+    #   parsed_json = JSON.parse(response.body, symbolize_names: true)
+    #   errors = parsed_json[:errors].first
+
+    #   expect(errors[:status]).to eq("404")
+    #   expect(errors[:message]).to include("Couldn't find Merchant with 'id'=99999")
+    # end
+
+    it 'also deletes associated items when merchant is deleted' do 
+      merchant = Merchant.create!(name: "Test Cascading")
+     
+      item1 = merchant.items.create!(name: "Item 1", description: "Born to be deleted", unit_price: 10)
+      item2 = merchant.items.create!(name: "Item 2", description: "Second born to be deleted", unit_price: 20)
+
+      intial_item_count = Item.count 
+      delete "/api/v1/merchants/#{merchant.id}"
+      final_item_count = Item.count 
+    
+      expect(final_item_count).to eq(intial_item_count - 2)
+    end
+  end
+end
