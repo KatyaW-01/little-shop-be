c9e134e42795732939d469185cd6571c1c19e339
Maia Bard <Maiakbard@gmail.com>
2025-04-17 15:35:03 -0600

    Add test for cascading deletion for merchant to check for deletion of item and invoice items accosiated with merchant

--- spec/requests/merchant_request_spec.rb
+++ spec/requests/merchant_request_spec.rb
@@ -23,17 +23,23 @@ RSpec.describe "Merchant endpoints", type: :request do
     #   expect(errors[:message]).to include("Couldn't find Merchant with 'id'=99999")
     # end
 
-    it 'also deletes associated items when merchant is deleted' do 
+    it 'also deletes associated items and accosiated invoice item when merchant is deleted' do 
       merchant = Merchant.create!(name: "Test Cascading")
      
       item1 = merchant.items.create!(name: "Item 1", description: "Born to be deleted", unit_price: 10)
       item2 = merchant.items.create!(name: "Item 2", description: "Second born to be deleted", unit_price: 20)
+      customer = Customer.create!(first_name: "Test", last_name: "Customer")
+      invoice = Invoice.create!(status: "shipped", customer_id: customer.id, merchant_id: merchant.id)
+      invoice_item = item1.invoice_items.create!(invoice: invoice, quantity: 5, unit_price: 100)
 
-      intial_item_count = Item.count 
+      initial_item_count = Item.count
+      initial_invoice_item_count = InvoiceItem.count 
       delete "/api/v1/merchants/#{merchant.id}"
       final_item_count = Item.count 
-    
-      expect(final_item_count).to eq(intial_item_count - 2)
+      final_invoice_item_count = InvoiceItem.count
+
+      expect(final_item_count).to eq(initial_item_count - 2)
+      expect(final_invoice_item_count).to eq(initial_invoice_item_count - 1)
     end
   end
 end
