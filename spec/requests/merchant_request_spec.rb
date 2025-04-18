3904d1fb35c3376db44d65e57b6879f098f64bde
Maia Bard <Maiakbard@gmail.com>
2025-04-18 15:47:55 -0600

    Add fixed tests for response on deletion of all realted data on merchant

--- spec/requests/merchant_request_spec.rb
+++ spec/requests/merchant_request_spec.rb
@@ -11,20 +11,20 @@ RSpec.describe "Merchant endpoints", type: :request do
       expect(Merchant.find_by(id: merchant.id)).to be_nil
     end
 
-    # it 'returns a not_found error' do
-    #   delete "/api/v1/merchants/99999"
+    xit 'returns a not_found error' do
+      delete "/api/v1/merchants/99999"
 
-    #   expect(response).to have_http_status(:not_found)
+      expect(response).to have_http_status(:not_found)
 
-    #   parsed_json = JSON.parse(response.body, symbolize_names: true)
-    #   errors = parsed_json[:errors].first
+      parsed_json = JSON.parse(response.body, symbolize_names: true)
+      errors = parsed_json[:errors].first
 
-    #   expect(parsed_json[:errors]).to be_an(Array)
-    #   expect(errors[:status]).to eq("404")
-    #   expect(errors[:message]).to include("Couldn't find Merchant with 'id'=99999")
-    # end
+      expect(parsed_json[:errors]).to be_an(Array)
+      expect(errors[:status]).to eq("404")
+      expect(errors[:message]).to include("Couldn't find Merchant with 'id'=99999")
+    end
 
-    it 'also deletes associated items and accosiated invoice item when merchant is deleted' do 
+    it 'also deletes associated items and accosiated invoice item when merchant is deleted' do
       merchant = Merchant.create!(name: "Test Cascading")
      
       item1 = merchant.items.create!(name: "Item 1", description: "Born to be deleted", unit_price: 10)
@@ -32,15 +32,24 @@ RSpec.describe "Merchant endpoints", type: :request do
       customer = Customer.create!(first_name: "Test", last_name: "Customer")
       invoice = Invoice.create!(status: "shipped", customer_id: customer.id, merchant_id: merchant.id)
       invoice_item = item1.invoice_items.create!(invoice: invoice, quantity: 5, unit_price: 100)
+      transaction = invoice.transactions.create!(credit_card_number: "1234567890123456", result: "success")
 
-      initial_item_count = Item.count
-      initial_invoice_item_count = InvoiceItem.count 
+      expect(Merchant.find_by(id: merchant.id)).to_not be_nil
+      expect(Invoice.find_by(id: invoice.id)).to_not be_nil
+      expect(Transaction.find_by(id: transaction.id)).to_not be_nil
+      expect(InvoiceItem.find_by(id: invoice_item.id)).to_not be_nil
+      expect(Item.find_by(id: item1.id)).to_not be_nil
+      expect(Item.find_by(id: item2.id)).to_not be_nil
+
       delete "/api/v1/merchants/#{merchant.id}"
-      final_item_count = Item.count 
-      final_invoice_item_count = InvoiceItem.count
 
-      expect(final_item_count).to eq(initial_item_count - 2)
-      expect(final_invoice_item_count).to eq(initial_invoice_item_count - 1)
+      expect(response).to have_http_status(:no_content)
+      expect(Merchant.find_by(id: merchant.id)).to be_nil
+      expect(Invoice.find_by(id: invoice.id)).to be_nil
+      expect(Transaction.find_by(id: transaction.id)).to be_nil
+      expect(Item.find_by(id: item1.id)).to be_nil
+      expect(Item.find_by(id: item2.id)).to be_nil
+      expect(InvoiceItem.find_by(id: invoice_item.id)).to be_nil
     end
   end
 end
