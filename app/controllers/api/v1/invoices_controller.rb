da20c7ec25a91817b1f42f8ca0739e7716cad163
Maia Bard <Maiakbard@gmail.com>
2025-04-20 02:28:04 -0600

    Add code to raise a custom error so an invalid input is caught and throws an error message

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -5,8 +5,12 @@ class Api::V1::InvoicesController < ApplicationController
 
   def index
     status = params[:status] #grabs the value of the status query parameter - shipped, packaged, returned
+
+    raise InvalidStatusError, "Invalid status..." if status.present? && !VALID_STATUSES.include?(status)
+
     merchant = Merchant.find(params[:merchant_id]) #looks up merchant with given merchant id frome the URL
-    invoices = merchant.invoices.where(status: status) #this will get all the merchants invoices that match the status being passed in
+    invoices = merchant.invoices #this will get all the merchants invoices
+    invoices = merchant.invoices.where(status: status) if status.present?#this will get all the merchants invoices that match the status being passed in
 
     render json: InvoiceSerializer.new(invoices)
   end
