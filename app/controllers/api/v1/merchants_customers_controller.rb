a4134d9481f33b2076741b9bf8bd9f45f670d13d
Maia Bard <Maiakbard@gmail.com>
2025-04-19 23:34:22 -0600

    Add rescue from for not found error to index method for merchant items controller file to help handle the error

--- app/controllers/api/v1/merchants_customers_controller.rb
+++ app/controllers/api/v1/merchants_customers_controller.rb
@@ -1,7 +1,13 @@
 class Api::V1::MerchantsCustomersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
   def index
-    merchant = Merchant.find_by(id: params[:merchant_id])
+    merchant = Merchant.find(params[:merchant_id])
     customers = Customer.for_merchant(merchant.id).limit(50)
     render json: CustomerSerializer.new(customers)
+  end
+
+  private
+  def not_found_response(exception)
+    render json: ErrorSerializer.serialize(exception), status: :not_found
   end
 end
