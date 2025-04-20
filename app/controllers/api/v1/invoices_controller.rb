a808290c9524ef7e21349af13fb513b0a23f2a24
David Kennedy <davidkennedy@Davids-MacBook-Air.local>
2025-04-19 23:22:25 -0600

    make error handler for invoices

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -1,9 +1,16 @@
 class Api::V1::InvoicesController < ApplicationController
+  rescue_from  ActiveRecord::RecordNotFound, with: :incomplete_response
   def index
     status = params[:status] #grabs the value of the status query parameter - shipped, packaged, returned
     merchant = Merchant.find(params[:merchant_id]) #looks up merchant with given merchant id frome the URL
     invoices = merchant.invoices.where(status: status) #this will get all the merchants invoices that match the status being passed in
 
     render json: InvoiceSerializer.new(invoices)
+
+  end
+  private
+
+  def incomplete_response(exception)
+    render json: ErrorSerializer.serialize(exception), status: :not_found
   end
 end
--- app/controllers/api/v1/merchants_customers_controller.rb
+++ app/controllers/api/v1/merchants_customers_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::MerchantsCustomersController < ApplicationController
   def index
     merchant = Merchant.find_by(id: params[:merchant_id])
     customers = Customer.for_merchant(merchant.id).limit(50)
+    binding.pry
     render json: CustomerSerializer.new(customers)
   end
 end
