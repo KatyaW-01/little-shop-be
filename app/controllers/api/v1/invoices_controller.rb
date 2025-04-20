ed2c704653aca4f978211bda64760a49cdf5a8ed
Maia Bard <Maiakbard@gmail.com>
2025-04-20 02:10:11 -0600

    Add class for invalid status and add a rescue from for the bad request a incorrect status will throw

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -1,11 +1,18 @@
 class Api::V1::InvoicesController < ApplicationController
   VALID_STATUSES = ["shipped", "returned", "packaged"]
-  
+
+  rescue_from InvalidStatusError, with: :invalid_status_error
+
   def index
     status = params[:status] #grabs the value of the status query parameter - shipped, packaged, returned
     merchant = Merchant.find(params[:merchant_id]) #looks up merchant with given merchant id frome the URL
     invoices = merchant.invoices.where(status: status) #this will get all the merchants invoices that match the status being passed in
 
     render json: InvoiceSerializer.new(invoices)
+  end
+
+  private
+  def invalid_status_error(exception)
+    render json: ErrorSerializer.serialize(exception), status: :bad_request
   end
 end
