488755c9c7ddc9c0134e5dd1df9355fb74dbb304
Maia Bard <Maiakbard@gmail.com>
2025-04-20 01:13:43 -0600

    Add validates presense for name, description, unit_price, and merchant_id to allow for an error to be thrown when any of those parameters are missing

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items, dependent: :destroy
-  validates :merchant, presence: true
+  # validates :merchant, presence: true
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :unit_price, presence: true
+  validates :merchant_id, presence: true
 
   def self.sorted_by_price
     order(:unit_price)
