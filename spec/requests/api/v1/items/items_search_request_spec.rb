7cd5978c81d9198ff80a3e664b4f6ad3dfd50e94
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-04-21 10:32:38 -0400

    Starts adding in RSpec test suite for new endpoint

--- spec/requests/api/v1/items/items_search_request_spec.rb
+++ spec/requests/api/v1/items/items_search_request_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe "Item Search API", type: :request do
   end
 
   # Happy Path
-  describe "GET /api/v1/items/find" do
+  describe "Happy Path GET /api/v1/items/find" do
     it "returns one item matching the name fragment" do
       get "/api/v1/items/find?name=ring"
 
@@ -26,14 +26,67 @@ RSpec.describe "Item Search API", type: :request do
       json = JSON.parse(response.body, symbolize_names: true)
       expect(json[:data][:attributes][:name].to eq("Gold Ring"))
     end
+  end
 
-    # Sad Path
+  describe "Sad Path - GET /api/v1/items/find" do
     it "returns error if no param is passed" do
       get "/api/v1/items/find"
 
       expect(response.status).to eq(400)
       json = JSON.parse(response.body, symbolize_names: true)
       expect(json[:errors]).to include("name or price range")
+    end
+
+    it "returns an error if name and price are both passed" do
+      get "api/v1/items/find?name=ring&min_price=5"
+
+      expect(response.status).to eq(400)
+      json = JSON.parse(response.body, symbolize_names: true)
+      expect(json[:errors]).to include("Cannot search by name and price simultaneously")
+    end
+
+    it "returns an error for negative price" do
+      get "/api/v1/items/find?min_price=-5"
+
+      expect(response.status).to eq(400)
+      json = JSON.parse(response.body, symbolize_names: true)
+      expect(json[:errors].first).to include("must be a non-negative value")
+    end
+  end
+
+  describe "Happy Path - GET /api/v1/items/find_all" do
+    it "returns all matching items by name fragment" do
+      get "/api/v1/items/find_all?name=ring"
+
+      expect(response).to be_successful
+      json = JSON.parse(response.body, symbolize_names: true)
+      
+      names = json[:data].map { |item| item[:attributes][:name] }
+
+      expect(names).to include("Gold Ring", "Silver Ring")
+      expect(names).not_to include("Wooden Spoon")
+    end
+
+    it "returns all items within price range" do
+      get "/api/v1/items/find_all?min_price100&max_price=250"
+
+      expect(response).to be_successful
+      json = JSON.parse(response.body, symbolize_names: true)
+      expect(json[:data].count).to eq(2)
+    end
+
+    it "returns an empty array if no match" do
+      get "/api/v1/items/find_all?name=NOMATCH"
+
+      expect(response).to be_successful
+      json = JSON.parse(response.body, symbolize_names: true)
+      expect(json[:data]).to eq([])
+    end
+  end
+
+  describe "Sad Path - GET /api/v1/items/find_all" do
+    it "returns an error for negative max_price" do
+      
     end
   end
 end
