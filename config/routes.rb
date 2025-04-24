1e71a331eb69c3c6d963a316a423d3765f1388ce
Katya Weicht <katya.weicht@gmail.com>
2025-04-24 10:57:25 -0600

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,11 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
   #non RESTful endpoints
   get "/api/v1/merchants/find", to: "api/v1/merchants#find"
+  get "/api/v1/items/find", to: "api/v1/items_search#find"
+  get "/api/v1/items/find_all", to: "api/v1/items_search#find_all"
+
   # Merchant Endpoints
   get "/api/v1/merchants", to: "api/v1/merchants#index"
   get "/api/v1/items", to: "api/v1/items#index"
@@ -18,11 +19,6 @@ Rails.application.routes.draw do
   delete "/api/v1/merchants/:id", to: "api/v1/merchants#destroy"
  
   # Item Endpoints
-
-  # Non-RESTful Find endpoints for Items
-  get "/api/v1/items/find", to: "api/v1/items_search#find"
-  get "/api/v1/items/find_all", to: "api/v1/items_search#find_all"
-  
   get "/api/v1/items", to: "api/v1/items#index"
   get "/api/v1/items/:id", to: "api/v1/items#show"
   post "/api/v1/items", to: "api/v1/items#create"
@@ -35,6 +31,5 @@ Rails.application.routes.draw do
   get "/api/v1/items/:id/merchant", to: "api/v1/item_merchants#show"
   get "/api/v1/merchants/:merchant_id/invoices", to: "api/v1/invoices#index"
   get "/api/v1/merchants/:merchant_id/customers", to: "api/v1/merchants_customers#index"
-
  
 end
