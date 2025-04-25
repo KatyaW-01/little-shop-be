62289f1b5b4ad9783b9b43d94faea5ca974b42d1
Katya Weicht <katya.weicht@gmail.com>
2025-04-25 00:07:31 -0600

    Write method to prevent update if coupon limit reached

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,7 +1,9 @@
 class Coupon < ApplicationRecord
+
   belongs_to :merchant
   validates :code, presence: true, uniqueness: true
   validate :check_coupon_limit
+  validate :check_coupon_count, on: :update
 
   def check_coupon_limit
     if activated? && Coupon.where(merchant_id: self.merchant_id, activated: true).count > 5
@@ -9,4 +11,11 @@ class Coupon < ApplicationRecord
     end
   end
   #checks if activated is true and if the count is greater than 5 
+
+  def check_coupon_count #accounts for updating the activated column to true 
+    if will_save_change_to_activated? && activated? && Coupon.where(merchant_id: self.merchant_id, activated: true).count > 5
+      errors.add(:activated, "You have reached the maximum number of activated coupons")
+    end
+  end
+  #will_save_change_to_activated checks if the activated column will be changing 
 end
