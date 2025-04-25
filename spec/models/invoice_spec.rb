4a4e4fd0576039a7c9d02d70bdfe11847cc29a94
Katya Weicht <katya.weicht@gmail.com>
2025-04-24 20:22:11 -0600

    Update invoice model and spec file

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 require "spec_helper"
 
 RSpec.describe Invoice, type: :model do
+  it { should belong_to(:coupon).optional}
   it { should belong_to :customer }
   it { should belong_to :merchant }
   it { should have_many :invoice_items }
