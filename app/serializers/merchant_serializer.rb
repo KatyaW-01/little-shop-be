9fa985a14ea3d835ccd613a16a29b96924c06a69
Katya Weicht <katya.weicht@gmail.com>
2025-04-25 23:50:02 -0600

    Add counts to merchant serializer

--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -1,5 +1,11 @@
 class MerchantSerializer
   include JSONAPI::Serializer
   attributes :name
+  attributes :coupons_count do |object|
+    object.coupons.count
+  end
+  attributes :invoice_coupon_count do |object|
+    object.invoices.where(:coupon_id => !nil).count
+  end
 end
 
